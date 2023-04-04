6e197d575bb757d3de05a4f308d8e8097a39e2b7
shufol islam <shufolislam1255@gmail.com>
2023-04-04 16:00:54 +0600

    first  week first problem solve with img of statement

---
+++ w1pd1/pp1.c
@@ -0,0 +1,20 @@
+/*
+    Statement:--------------------------------
+
+    You need to print the lines given below in the same format as it is -
+
+“Recently I heard that you’ve achieved 95% marks in your exam.
+This is brilliant!
+I wish you’ll shine in your life!	Good luck with all the barriers(/\) in your life.”
+
+*/
+#include<stdio.h>
+
+int main(){
+
+    printf("Recently I heard that you%cve achieved 95%% marks in your exam.\nThis is brilliant!\nI wish you%cll shine in your life!\tGood luck with all the barriers(/\\) in your life.", 39, 39);
+
+    return 0;
+}
+
+//main learning of this code is that have to print single quote with ascii value of 39.
